c692aa2b274db15a25b7fed7b1dbc37edf0bbe63
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-03-18 23:17:48 +0530

    start

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/ipriyam26/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
+
---
+++ fish/functions/br.fish
@@ -0,0 +1,1 @@
+/Users/ipriyam26/Library/Application Support/org.dystroy.broot/launcher/fish/br.fish
